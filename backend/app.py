f17befc5b682df5c093b55c29ff6aded0d55a906
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-22 05:50:48 +0100

    Added validation.

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request, jsonify
-import validator
+from flask import Flask, request, jsonify, make_response
 import statistics
 app = Flask(__name__)             # create an app instance
 
@@ -23,22 +22,37 @@ def validate_gyroscope(gyroscope_data):
     return variance_data([x[0] for x in gyroscope_data]) + \
         variance_data([y[1] for y in gyroscope_data])
 
-def validate_location()
 
-@app.route('/validate', methods=['POST'])
+def corsify(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+def _build_cors_prelight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+
+@app.route('/validate', methods=['POST', 'OPTIONS'])
 def validate():
-    print(request.json)
+    if request.method == 'OPTIONS':
+        return _build_cors_prelight_response()
+    elif request.method == 'POST':
+        print(request.json)
 
-    mouse_score = validate_mouse_data(request.json['mouse'])
+        mouse_score = validate_mouse_data(request.json['mouse'])
 
-    gyroscope_score = 1
-    if "gyroscope" in request.json:
-        gyroscope_score = validate_gyroscope(request.json['gyroscope'])
+        gyroscope_score = 1
+        if "gyroscope" in request.json:
+            gyroscope_score = validate_gyroscope(request.json['gyroscope'])
 
-    return jsonify({
-        "mouse": mouse_score,
-        "gyroscope": gyroscope_score
-    })
+        return corsify(jsonify({
+            "mouse": mouse_score,
+            "gyroscope": gyroscope_score
+        }))
 
 
 if __name__ == "__main__":        # on running python app.py
--- frontend/src/components/SecureSlider.tsx
+++ frontend/src/components/SecureSlider.tsx
@@ -6,27 +6,59 @@ interface IProps {
 
 interface IState {
   progress: number;
+  validating: boolean;
   validated: boolean;
+  mouse_record;
 }
 
 class SecureSlider extends React.Component<IProps, IState> {
   constructor(props) {
     super(props);
-    this.state = { progress: 0.0, validated: false };
+    this.state = { progress: 0.0, validating: false, validated: false, mouse_record: [] };
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  doValidation()
+  {
+    let parent = this;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", 'http://127.0.0.1:5000/validate', true);
+
+    //Envoie les informations du header adaptées avec la requête
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () { //Appelle une fonction au changement d'état.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        parent.setState({ validated: true });
+        console.log(this.response);
+      }
+    }
+
+    let payload = {
+      "mouse": parent.state.mouse_record,
+    };
+
+    xhr.send(JSON.stringify(payload));
   }
 
   handleChange(event)
   {
     if (parseInt(event.target.value) >= this.state.progress)
     {
-      this.setState({ progress: event.target.value });
+      this.setState({ progress: parseInt(event.target.value) });
     }
 
-    if (this.state.progress >= 100)
+    if (this.state.progress >= 98)
     {
-      this.setState({ validated: true });
+      this.setState({ validating: true });
+      this.doValidation();
     }
+  }
+
+  handleMouseMove(event)
+  {
+    this.setState({ mouse_record: this.state.mouse_record.concat([[event.nativeEvent.offsetX, event.nativeEvent.offsetY]]) });
   }
 
   render() {
@@ -38,6 +70,13 @@ class SecureSlider extends React.Component<IProps, IState> {
           Looking good!
         </div>;
     }
+    else if (this.state.validating)
+    {
+      content =
+        <div className="validating">
+          Validating...
+        </div>;
+    }
     else
     {
       content =<div>
@@ -47,7 +86,7 @@ class SecureSlider extends React.Component<IProps, IState> {
           <div className="instruction">
             Slide to prove it!
           </div>
-          <input type="range" value={this.state.progress} onChange={this.handleChange} min="0" max="101" className="slider" id="myRange" />
+        <input type="range" onMouseMove={this.handleMouseMove.bind(this)} value={this.state.progress} onChange={this.handleChange} min="0" max="101" className="slider" id="myRange" />
           <div className="powered">
             powered by INABEB
           </div>
