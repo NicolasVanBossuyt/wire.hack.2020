0e16719254b0ffb948f10c6eed13d74edfa037df
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2020-02-22 04:22:37 +0100

    Updated the slider to be cool :)

--- frontend/src/components/SecureSlider.tsx
+++ frontend/src/components/SecureSlider.tsx
@@ -1,9 +1,43 @@
 import * as React from 'react';
 import './SecureSlider.scss';
 
-class SecureSlider extends React.Component {
+interface IProps {
+}
+
+interface IState {
+  progress: number;
+}
+
+class SecureSlider extends React.Component<IProps, IState> {
+  constructor(props) {
+    super(props);
+    this.state = { progress: 0.0 };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event)
+  {
+    if (parseInt(event.target.value) >= this.state.progress)
+    {
+      this.setState({ progress: event.target.value });
+    }
+  }
+
   render() {
-    return (<input type="range" min="1" max="100" className="slider" id="myRange"/>);
+    return (
+      <div className="wrapper">
+        <div className="question">
+          I’m not a brain eating bastard?
+        </div>
+        <div className="instruction">
+          Slide to prove it!
+        </div>
+        <input type="range" value={this.state.progress} onChange={this.handleChange} min="0" max="100" className="slider" id="myRange" />
+        <div className="powered">
+          powered by INABEB
+        </div>
+      </div>
+    );
   }
 }
 
